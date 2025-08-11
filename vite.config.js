178479481d7c5a53f9e999ec7f8844a06d6e5460
laptop2022gtav-blip <laptop2022gtav@gmail.com>
2025-08-11 16:29:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/uk-traffic-viewer/', // ✅ Matches your repo name exactly
+  base: '/uk-traffic-viewer/', // 👈 Add this for GitHub Pages
 });
-
-
