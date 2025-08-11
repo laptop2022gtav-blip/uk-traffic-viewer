d34bbc3db8574879992b6834b6c330992daeadf0
laptop2022gtav-blip <laptop2022gtav@gmail.com>
2025-08-11 23:43:56 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+//import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+//import { BrowserRouter } from "react-router-dom";
+//import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//ReactDOM.createRoot(document.getElementById("root")).render(
+     //<BrowserRouter basename="/uk-traffic-viewer">
+    //<App />
+     //</BrowserRouter>
+//);
+
+createRoot(document.getElementById("root")).render(<App />);
